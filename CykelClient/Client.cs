39b124317e1d6d1edfac62250b471185ac8d0355
Marc Steen Fordsmand <marc5517@edu.easj.dk>
2020-09-30 15:29:02 +0200

    Sat nyt projekt med Client

---
+++ CykelClient/Client.cs
@@ -0,0 +1,32 @@
+﻿using CykelLib;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+
+namespace CykelClient
+{
+    class Client
+    {
+        public void Start()
+        {
+            TcpClient socket = new TcpClient("localhost", 4646);
+
+            StreamReader sr = new StreamReader(socket.GetStream());
+            StreamWriter sw = new StreamWriter(socket.GetStream());
+
+
+            Cyklen cyklen = new Cyklen(123, "Gul", 4700, 7);
+            String json = JsonConvert.SerializeObject(cyklen);
+
+
+            sw.WriteLine("Gem");
+            sw.WriteLine(json);
+            sw.Flush();
+
+            socket.Close();
+        }
+    }
+}
