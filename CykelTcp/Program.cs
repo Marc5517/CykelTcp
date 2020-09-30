7f00286811b57f338580dee472fca2e11a630d29
Marc Steen Fordsmand <marc5517@edu.easj.dk>
2020-09-30 15:12:08 +0200

    Add project files.

---
+++ CykelTcp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CykelTcp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
