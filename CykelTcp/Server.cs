85a3ee3297757947fbb2b9ea3cb26e59358efbe5
Marc Steen Fordsmand <marc5517@edu.easj.dk>
2020-09-30 15:23:21 +0200

    Lavet library og klasse

--- CykelTcp/Server.cs
+++ CykelTcp/Server.cs
@@ -1,6 +1,13 @@
-﻿using System;
+﻿using CykelLib;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace CykelTcp
 {
